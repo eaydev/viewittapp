0c72824e9a359e071fdaed0f29f01766132c2db4
Ephraim Amiel Yusi <ephraimyusi@gmail.com>
2020-04-24 20:25:41 +1000

    Link change.

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -4,7 +4,7 @@ export default class Footer extends Component{
   render(){
     return(
       <div className="container-fluid text-center p-2 pt-3">
-        <p>Made with <span style={{color: 'red', fontSize: '1.4em'}}>&#x2665;</span> by <a href="https://twitter.com/eaydev">eaydev</a></p>
+        <p>Made with <span style={{color: 'red', fontSize: '1.4em'}}>&#x2665;</span> by <a href="https://www.reddit.com/user/fin_gw">eaydev</a></p>
       </div>
     )
   }
